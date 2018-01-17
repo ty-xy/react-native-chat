9a5ba2f411b7ae67a049fc18d8dd9172572c2297
guowentao <zg-gwt@zg18.com>
2018-01-17 09:45:54 +0800

    feat: 相机翻转

--- app/view/common/Camera.js
+++ app/view/common/Camera.js
@@ -5,7 +5,7 @@ import {
     StyleSheet,
     Text,
     View,
-    TouchableHighlight,
+    TouchableOpacity,
     StatusBar
 } from 'react-native';
 import Camera from 'react-native-camera';
@@ -34,33 +34,32 @@ export default class MyCamera extends PureComponent {
     }
 
     //切换前后摄像头
-    switchCamera() {
-        var state = this.state;
-        if(state.cameraType === Camera.constants.Type.back) {
-            state.cameraType = Camera.constants.Type.front;
+    switchCamera = () => {
+        var cameraType = this.state.cameraType;
+        if(cameraType === Camera.constants.Type.back) {
+            cameraType = Camera.constants.Type.front;
         }else{
-            state.cameraType = Camera.constants.Type.back;
-        }
-        this.setState(state);
+            cameraType = Camera.constants.Type.back;
+        }  
+        this.setState({ cameraType });
     }
     _goBack = () => {
         this.props.navigation.goBack()
     }
     //拍摄照片
-    takePicture() {
+    takePicture = () => {
         this.camera.capture()
-        .then(function(data){
-            alert("拍照成功！图片保存地址：\n"+data.path)
-        })
-        .catch(err => console.error(err));
+            .then(function(data){
+                alert("拍照成功！图片保存地址：\n"+data.path)
+            })
+            .catch(err => console.error(err));
     }
     render() {
         const { style = {}, lastMessage, _id, name } = this.props;
-
         return (
             <View style={styles.container}>
                 <StatusBar hidden={true} />
-                <Text style={styles.cancel} onPress={this._goBack}>取消</Text>
+                <Text style={styles.back} onPress={this.switchCamera}>&#xe642;</Text>
                 <Camera
                     ref={(cam) => {
                         this.camera = cam;
@@ -69,8 +68,16 @@ export default class MyCamera extends PureComponent {
                     type={this.state.cameraType}
                     aspect={Camera.constants.Aspect.fill}
                 >
-                    <Text style={styles.button} onPress={this.takePicture.bind(this)}>[拍照]</Text>
-                    <Text style={styles.button} onPress={this.switchCamera.bind(this)}>[切换摄像头]</Text>
+                    <Text style={[styles.goback]} onPress={this.takePicture} onPress={this._goBack}>&#xe63b;</Text>
+                    <TouchableOpacity
+                        activeOpacity={0.6}
+                        style={styles.button}
+                        onPress={this.takePicture}
+
+                    >
+                        <View style={styles.button2}></View>
+                    </TouchableOpacity>
+                    <View />
                 </Camera>
             </View> 
         );
@@ -88,33 +95,49 @@ const styles = StyleSheet.create({
         flex: 1,
         flexDirection: 'row',
     },
-    cancel: {
+    back: {
+        fontFamily: 'iconfont',
         color: '#fff',
-        fontSize: 20,
+        backgroundColor: 'transparent',
+        fontSize: 30,
         position: 'absolute',
         top: 15,
-        left: 15,
-        zIndex: 2
+        right: 15,
+        zIndex: 3,
+        padding: 10,
     },
     preview: {
         flex: 1,
-        justifyContent: 'space-between',
+        justifyContent: 'center',
         alignItems: 'flex-end',
         flexDirection: 'row',
+        position: 'relative',
     },
-    toolBar: {
-        width: 200,
-        margin: 40,
-        backgroundColor: '#000000',
-        justifyContent: 'space-between',
-     
+    goback: {
+        fontFamily: 'iconfont',
+        color: '#fff',
+        backgroundColor: 'transparent',
+        transform: [{ rotateZ: '90deg' }],
+        fontSize: 40,
+        position: 'absolute',
+        left: 50,
+        bottom: 88,
+        textAlign: 'center',
+        alignSelf: 'center',
     },
     button: {
-        flex: 0,
-        backgroundColor: '#fff',
-        borderRadius: 5,
-        color: '#000',
-        padding: 10,
-        margin: 40,
+        backgroundColor: 'rgba(255,255,255,0.25)',
+        height: 66,
+        width: 66,
+        borderRadius: 33,
+        marginBottom: 80,
+        justifyContent: 'center',
+        alignItems: 'center'
     },
+    button2: {
+        backgroundColor: '#fff',
+        height: 46,
+        width: 46,
+        borderRadius: 23,
+    }
 });
