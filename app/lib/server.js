a4184aa74b58c6346f034e5a28fec3d51c6c250a
guowentao <zg-gwt@zg18.com>
2018-01-29 10:26:51 +0800

    change server

--- app/lib/server.js
+++ app/lib/server.js
@@ -3,7 +3,7 @@ import Meteor from 'react-native-meteor';
 export default () => {
     // Meteor.connect('ws://127.0.0.1:3000/websocket');
     // const url = 'wss://www.zg18.com:13220/websocket';
-    const url = 'ws://192.168.1.102:3000/websocket';
+    const url = 'ws://192.168.1.142:3000/websocket';
     
     Meteor.connect(url);
 }
